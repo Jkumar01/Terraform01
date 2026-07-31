01b9c61ca92b114ee3ffc982be5b87410b3a68db
Jkumar01 <Jaideep1207@yahoo.com>
2026-07-31 15:00:15 +0530

    Added Backend Config

---
+++ Terrafrom/backend/backend-config.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "cde-terraform-state-dev"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+    use_lockfile   = true
+  }
+
+}
